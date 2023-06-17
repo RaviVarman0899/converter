50e168d75512ecea72c39a30c22564ee77a4d6dc
RaviVarman0899 <135153170+RaviVarman0899@users.noreply.github.com>
2023-06-16 22:01:01 -0400

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -8,17 +8,14 @@ function convert() {
     let usdOption = document.getElementById("usdCurrency").value;
     let cadOption = document.getElementById("cadCurrency").value;
 
-    if (usdOption == cadOption) {
-        alert("Cannot convert to same currency value!")
-    }
-    else if (usd == null || usd == undefined || usd == "") {
+    if (usd == null || usd == undefined || usd == "") {
         alert("The input value is empty, please provide a value to convert.")
     }
     else if (usdOption == "CAD") {
         document.getElementById("CAD").value = parseFloat(usd * 0.756);
     }
     else if (usdOption == "USD") {
-        document.getElementById("CAD").value = parseFloat(usd * 1.322);
+        document.getElementById("CAD").value = parseFloat(usd * 1.322)
     }
 
 }
